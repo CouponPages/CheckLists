e2b69bc3948c45aebedb66db80da19ed41450f39
Joe Crescenzi <CouponPages@gmail.com>
2017-09-23 09:01:18 -0400

    Break for breakfast... just saving.  Has some dummy data as a lesson in how IndexPath.row works.

--- CheckLists/ChecklistViewController.swift
+++ CheckLists/ChecklistViewController.swift
@@ -29,13 +29,28 @@ class ChecklistViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 7
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
         
-      
+        // Add the following code
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0
+            { label.text = "Walk the dog" }
+        else if indexPath.row == 1
+            { label.text = "Brush my teeth" }
+        else if indexPath.row == 2
+            { label.text = "Learn iOS development" }
+        else if indexPath.row == 3
+            { label.text = "Soccer practice" }
+        else if indexPath.row == 4
+            { label.text = "Eat ice cream" }
+        // End of new code block
+        
+
         return cell
     }
 
