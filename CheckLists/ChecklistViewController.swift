fc3bbe9dae647ad563ef36d135ecd57fc69e3e73
Joe Crescenzi <CouponPages@gmail.com>
2017-09-24 07:51:06 -0400

    End of page 219.. swiped hard coded text with variables.

--- CheckLists/ChecklistViewController.swift
+++ CheckLists/ChecklistViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ChecklistViewController: UITableViewController
 {
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
+    
+    
+    
 
     override func viewDidLoad()
     {
@@ -33,7 +41,29 @@ class ChecklistViewController: UITableViewController
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of rows
-        return 100
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        // Add the following code
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row == 0
+        { label.text = row0text }
+        else if indexPath.row == 1
+        { label.text = row1text}
+        else if indexPath.row == 2
+        { label.text = row2text }
+        else if indexPath.row == 3
+        { label.text = row3text }
+        else if indexPath.row == 4
+        { label.text = row4text}
+
+        
+        
+        return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
@@ -55,27 +85,6 @@ class ChecklistViewController: UITableViewController
     }
     
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        
-        // Add the following code
-        let label = cell.viewWithTag(1000) as! UILabel
-        
-        if indexPath.row % 5 == 0
-            { label.text = "Walk the dog" }
-        else if indexPath.row % 5 == 1
-            { label.text = "Brush my teeth" }
-        else if indexPath.row % 5 == 2
-            { label.text = "Learn iOS development" }
-        else if indexPath.row % 5 == 3
-            { label.text = "Soccer practice" }
-        else if indexPath.row % 5 == 4
-            { label.text = "Eat ice cream" }
-        // End of new code block
-        
-
-        return cell
-    }
 
     
     
