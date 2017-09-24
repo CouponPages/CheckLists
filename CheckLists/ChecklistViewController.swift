2ba5c3e126b0ab7a0581ebb93e9ff59f73f20ef1
Joe Crescenzi <CouponPages@gmail.com>
2017-09-24 08:43:35 -0400

    Page 237... uses arrays of item objects (loaded by hand). Still works.

--- CheckLists/ChecklistViewController.swift
+++ CheckLists/ChecklistViewController.swift
@@ -10,35 +10,39 @@ import UIKit
 
 class ChecklistViewController: UITableViewController
 {
-    var row0item: ChecklistItem
-    var row1item: ChecklistItem
-    var row2item: ChecklistItem
-    var row3item: ChecklistItem
-    var row4item: ChecklistItem
+    var items: [ChecklistItem]
+    
     
 
     required init?(coder aDecoder: NSCoder)
     {
-        row0item = ChecklistItem()
+        items = [ChecklistItem]()
+        
+        let row0item = ChecklistItem()
         row0item.text = "Walk the dog"
         row0item.checked = false
+        items.append(row0item)
         
-        row1item = ChecklistItem()
+        let row1item = ChecklistItem()
         row1item.text = "Brush my teeth"
         row1item.checked = true
-        
-        row2item = ChecklistItem()
+        items.append(row1item)
+
+        let row2item = ChecklistItem()
         row2item.text = "Learn iOS development"
         row2item.checked = true
-        
-        row3item = ChecklistItem()
+        items.append(row2item)
+
+        let row3item = ChecklistItem()
         row3item.text = "Soccer practice"
         row3item.checked = false
-        
-        row4item = ChecklistItem()
+        items.append(row3item)
+
+        let row4item = ChecklistItem()
         row4item.text = "Eat ice cream"
         row4item.checked = true
-        
+        items.append(row4item)
+
         super.init(coder: aDecoder)
     }
     
@@ -71,32 +75,25 @@ class ChecklistViewController: UITableViewController
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        
-        // Add the following code
+        let item = items[indexPath.row]
         let label = cell.viewWithTag(1000) as! UILabel
-        if indexPath.row == 0 { label.text = row0item.text }
-        else if indexPath.row == 1 { label.text = row1item.text }
-        else if indexPath.row == 2 { label.text = row2item.text }
-        else if indexPath.row == 3 { label.text = row3item.text }
-        else if indexPath.row == 4 { label.text = row4item.text }
         
+        label.text = item.text
         configureCheckmark(for: cell, at: indexPath)
         return cell
     }
 
     func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath)
     {
-        var isChecked = false
-        
-        if indexPath.row == 0 { isChecked = row0item.checked }
-        else if indexPath.row == 1 { isChecked = row1item.checked }
-        else if indexPath.row == 2 { isChecked = row2item.checked }
-        else if indexPath.row == 3 { isChecked = row3item.checked }
-        else if indexPath.row == 4 { isChecked = row4item.checked }
-        
-        if isChecked { cell.accessoryType = .checkmark } else { cell.accessoryType = .none }
-        
-        
+        let item = items[indexPath.row]
+        if item.checked
+        {
+            cell.accessoryType = .checkmark
+        }
+        else
+        {
+            cell.accessoryType = .none
+        }
     }
 
     
@@ -106,14 +103,10 @@ class ChecklistViewController: UITableViewController
     {
         if let cell = tableView.cellForRow(at: indexPath)
         {
-            if indexPath.row == 0 { row0item.checked = !row0item.checked }
-            else if indexPath.row == 1 { row1item.checked = !row1item.checked }
-            else if indexPath.row == 2 { row2item.checked = !row2item.checked }
-            else if indexPath.row == 3 { row3item.checked = !row3item.checked }
-            else if indexPath.row == 4 { row4item.checked = !row4item.checked }
+            let item = items[indexPath.row]
+            item.checked = !item.checked
             
             configureCheckmark(for: cell, at: indexPath)
-            
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
